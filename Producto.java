f81188bd9cd944a66240e3cb5d7f8cb5876a47c9
Moises soler <38496433+jamososoler@users.noreply.github.com>
2024-02-16 11:12:12 -0500

    subiendo archivo java

---
+++ Producto.java
@@ -0,0 +1,24 @@
+package en.notas.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idproducto;
+    String descripcion;
+    Double precio;
+    Integer existencia;
+}
